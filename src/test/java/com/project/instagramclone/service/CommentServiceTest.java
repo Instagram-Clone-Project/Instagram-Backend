60ecf134f01f10cfc35cd4aef477bce4934d515d
SeungCheol <ssc6839@gmail.com>
2021-09-20 18:40:26 +0900

    gitignore test

--- src/test/java/com/project/instagramclone/service/CommentServiceTest.java
+++ src/test/java/com/project/instagramclone/service/CommentServiceTest.java
@@ -1,115 +1,115 @@
-//package com.project.instagramclone.service;
-//
-//import com.project.instagramclone.domain.reply.entity.Comment;
-//import com.project.instagramclone.domain.reply.repository.CommentRepository;
-//import com.project.instagramclone.web.comment.dto.CommentSaveDto;
-//import com.project.instagramclone.web.comment.dto.CommentUpdateDto;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//import static org.assertj.core.api.Assertions.*;
-//
-//@SpringBootTest
-//class CommentServiceTest {
-//
-//    @Autowired
-//    private CommentRepository commentRepository;
-//    @Autowired
-//    private CommentService commentService;
-//
+package com.project.instagramclone.service;
+
+import com.project.instagramclone.domain.reply.entity.Comment;
+import com.project.instagramclone.domain.reply.repository.CommentRepository;
+import com.project.instagramclone.web.comment.dto.CommentSaveDto;
+import com.project.instagramclone.web.comment.dto.CommentUpdateDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+class CommentServiceTest {
+
+    @Autowired
+    private CommentRepository commentRepository;
+    @Autowired
+    private CommentService commentService;
+
+    @Test
+    public void test(){
+
+    }
+     //21-09-16 : 팀원들이 user post 완성하면 그거 넣어서 테스트해보기
+    @Test
+    public void commentSave() throws Exception{
+        //given
+
+        CommentSaveDto commentSaveDto = new CommentSaveDto();
+        commentSaveDto.setContent("첫번째 댓글");
+
+        // when
+        Comment comment = commentService.commentSave(commentSaveDto);
+        Comment saveComment = commentRepository.findById(comment.getId()).get();
+
+        // then
+
+        assertThat(comment.getContent()).isEqualTo(saveComment.getContent());
+        assertThat(comment.getId()).isEqualTo(saveComment.getId());
+    }
+
+    @Test
+    public void commentUpdate() throws Exception{
+        //given
+        CommentSaveDto commentSaveDto = new CommentSaveDto();
+        commentSaveDto.setContent("수정 전");
+
+        CommentUpdateDto commentUpdateDto = new CommentUpdateDto();
+        commentUpdateDto.setContent("수정 후");
+        // when
+
+        Comment before = commentService.commentSave(commentSaveDto);
+        System.out.println(before.getId());
+        Comment after = commentService.findById(before.getId());
+
+        commentService.commentUpdate(before.getId(),commentUpdateDto);
+        commentRepository.flush();
+
+        Comment comment = commentService.findById(before.getId());
+        // then
+
+        assertThat(comment.getContent()).isEqualTo(commentUpdateDto.getContent());
+    }
+
+    @Test
+    public void commentDelete() throws Exception{
+        //given
+        CommentSaveDto commentSaveDto = new CommentSaveDto();
+        commentSaveDto.setContent("첫번째 댓글");
+
+        // when
+        //Comment comment = commentService.commentSave(commentSaveDto);
+        commentRepository.delete(comment);
+        List<Comment> list = commentRepository.findAll();
+        // then
+
+
+        assertThat(list.size()).isEqualTo(0);
+    }
+
+
 //    @Test
-//    public void test(){
-//
-//    }
-//     //21-09-16 : 팀원들이 user post 완성하면 그거 넣어서 테스트해보기
-//    @Test
-//    public void commentSave() throws Exception{
+//    @Transactional
+//    public void nestedCommentSave() throws Exception{
 //        //given
+//        CommentSaveDto commentParent = new CommentSaveDto();
+//        commentParent.setContent("부모 댓글");
 //
-//        CommentSaveDto commentSaveDto = new CommentSaveDto();
-//        commentSaveDto.setContent("첫번째 댓글");
+//
+//        CommentSaveDto commentChildren = new CommentSaveDto();
+//        commentChildren.setContent("자식 댓글 (대댓글)");
+//
 //
 //        // when
-//        Comment comment = commentService.commentSave(commentSaveDto);
-//        Comment saveComment = commentRepository.findById(comment.getId()).get();
-//
-//        // then
-//
-//        assertThat(comment.getContent()).isEqualTo(saveComment.getContent());
-//        assertThat(comment.getId()).isEqualTo(saveComment.getId());
-//    }
-//
-//    @Test
-//    public void commentUpdate() throws Exception{
-//        //given
-//        CommentSaveDto commentSaveDto = new CommentSaveDto();
-//        commentSaveDto.setContent("수정 전");
-//
-//        CommentUpdateDto commentUpdateDto = new CommentUpdateDto();
-//        commentUpdateDto.setContent("수정 후");
-//        // when
-//
-//        Comment before = commentService.commentSave(commentSaveDto);
-//        System.out.println(before.getId());
-//        Comment after = commentService.findById(before.getId());
-//
-//        commentService.commentUpdate(before.getId(),commentUpdateDto);
+//        Comment parent = commentService.commentSave(commentParent);
+//        Comment children = commentService.nestedCommentSave(parent.getId(),commentChildren);
 //        commentRepository.flush();
 //
-//        Comment comment = commentService.findById(before.getId());
-//        // then
-//
-//        assertThat(comment.getContent()).isEqualTo(commentUpdateDto.getContent());
-//    }
-//
-//    @Test
-//    public void commentDelete() throws Exception{
-//        //given
-//        CommentSaveDto commentSaveDto = new CommentSaveDto();
-//        commentSaveDto.setContent("첫번째 댓글");
-//
-//        // when
-//        //Comment comment = commentService.commentSave(commentSaveDto);
-//        commentRepository.delete(comment);
-//        List<Comment> list = commentRepository.findAll();
+//        Comment parentToChild = parent.getChildren().get(0);
+//        Comment childToParent = children.getParent();
 //        // then
 //
 //
-//        assertThat(list.size()).isEqualTo(0);
+//        assertThat(parentToChild.getId()).isEqualTo(children.getId());
+//        assertThat(parentToChild.getContent()).isEqualTo(children.getContent());
+//
+//        assertThat(childToParent.getId()).isEqualTo(parent.getId());
+//        assertThat(childToParent.getContent()).isEqualTo(parent.getContent());
 //    }
-//
-//
-////    @Test
-////    @Transactional
-////    public void nestedCommentSave() throws Exception{
-////        //given
-////        CommentSaveDto commentParent = new CommentSaveDto();
-////        commentParent.setContent("부모 댓글");
-////
-////
-////        CommentSaveDto commentChildren = new CommentSaveDto();
-////        commentChildren.setContent("자식 댓글 (대댓글)");
-////
-////
-////        // when
-////        Comment parent = commentService.commentSave(commentParent);
-////        Comment children = commentService.nestedCommentSave(parent.getId(),commentChildren);
-////        commentRepository.flush();
-////
-////        Comment parentToChild = parent.getChildren().get(0);
-////        Comment childToParent = children.getParent();
-////        // then
-////
-////
-////        assertThat(parentToChild.getId()).isEqualTo(children.getId());
-////        assertThat(parentToChild.getContent()).isEqualTo(children.getContent());
-////
-////        assertThat(childToParent.getId()).isEqualTo(parent.getId());
-////        assertThat(childToParent.getContent()).isEqualTo(parent.getContent());
-////    }
-//}
+}
--- src/test/java/com/project/instagramclone/service/MemberServiceTest.java
+++ src/test/java/com/project/instagramclone/service/MemberServiceTest.java
@@ -1,56 +1,56 @@
-//package com.project.instagramclone.service;
-//
-//import com.project.instagramclone.domain.member.Member;
-//import com.project.instagramclone.domain.member.MemberRepository;
-//import com.project.instagramclone.web.member.dto.SignUpRequestDto;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//
-//import java.util.Optional;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//
-//@SpringBootTest
-//class MemberServiceTest {
-//
-//    @Autowired
-//    private MemberService memberService;
-//
-//    @Autowired
-//    private MemberRepository memberRepository;
-//
-//    @Autowired
-//    private PasswordEncoder passwordEncoder;
-//
-//    @Test
-//    public void 회원가입() throws Exception {
-//
-//        //given
-//        String email = "Test@Test.com";
-//        String memberName = "Shin";
-//        String id = "test_shin";
-//        String password = "test1234!";
-//
-//        SignUpRequestDto signUpRequestDto = SignUpRequestDto.builder()
-//                .email(email)
-//                .name(memberName)
-//                .id(id)
-//                .password(password)
-//                .build();
-//
-//        //when
-//        Long mno = memberService.signUp(signUpRequestDto);
-//
-//        Optional<Member> member = memberRepository.findById(mno);
-//
-//        //then
-//        boolean encodeResult = passwordEncoder.matches(password, member.get().getPassword());
-//
-//        assertThat(member.get().getEmail()).isEqualTo(email);
-//        assertThat(member.get().getName()).isEqualTo(memberName);
-//        assertThat(member.get().getId()).isEqualTo(id);
-//        assertThat(encodeResult).isEqualTo(true);
-//    }
-//}
+package com.project.instagramclone.service;
+
+import com.project.instagramclone.domain.member.Member;
+import com.project.instagramclone.domain.member.MemberRepository;
+import com.project.instagramclone.web.member.dto.SignUpRequestDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class MemberServiceTest {
+
+    @Autowired
+    private MemberService memberService;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Test
+    public void 회원가입() throws Exception {
+
+        //given
+        String email = "Test@Test.com";
+        String memberName = "Shin";
+        String id = "test_shin";
+        String password = "test1234!";
+
+        SignUpRequestDto signUpRequestDto = SignUpRequestDto.builder()
+                .email(email)
+                .name(memberName)
+                .id(id)
+                .password(password)
+                .build();
+
+        //when
+        Long mno = memberService.signUp(signUpRequestDto);
+
+        Optional<Member> member = memberRepository.findById(mno);
+
+        //then
+        boolean encodeResult = passwordEncoder.matches(password, member.get().getPassword());
+
+        assertThat(member.get().getEmail()).isEqualTo(email);
+        assertThat(member.get().getName()).isEqualTo(memberName);
+        assertThat(member.get().getId()).isEqualTo(id);
+        assertThat(encodeResult).isEqualTo(true);
+    }
+}
