c4659180cd8f2e9d492ba3fe28d16abd4df104f5
모수혁 <48712198+Mosuhyeok@users.noreply.github.com>
2021-09-26 20:08:47 +0900

    swagger 설정

--- src/main/java/com/project/instagramclone/web/comment/controller/CommentController.java
+++ src/main/java/com/project/instagramclone/web/comment/controller/CommentController.java
@@ -5,16 +5,19 @@ import com.project.instagramclone.security.UserDetailsImpl;
 import com.project.instagramclone.service.CommentService;
 import com.project.instagramclone.web.comment.dto.CommentSaveDto;
 import com.project.instagramclone.web.comment.dto.CommentUpdateDto;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@Api(tags = {"댓글"})
 public class CommentController {
 
     private final CommentService commentService;
-
+    @ApiOperation(value = "댓글 작성", notes = "댓글 작성입니다. {post_id}에는 댓글을 작성할 게시글 pk값입니다.")
     @PostMapping("/api/comment/{post_id}")
     public void commentSave(@AuthenticationPrincipal UserDetailsImpl userDetails,
                                 @PathVariable("post_id") Long post_id, @RequestBody CommentSaveDto commentSaveDto){
@@ -23,12 +26,13 @@ public class CommentController {
         commentService.commentSave(user,post_id,commentSaveDto);
     }
 
+    @ApiOperation(value = "댓글 수정", notes = "댓글 수정입니다. {comment_id}에는 댓글을 수정할 댓글 pk값입니다.")
     @PutMapping("/api/comment/{comment_id}")
     public void commentUpdate(@PathVariable("comment_id") Long commentId, @RequestBody CommentUpdateDto updateDto){
 
         commentService.commentUpdate(commentId,updateDto);
     }
-
+    @ApiOperation(value = "댓글 삭제", notes = "댓글 삭제입니다. {comment_id}에는 댓글을 삭제할 댓글 pk값입니다.")
     @DeleteMapping("/api/comment/{comment_id}")
     public void commentDelete(@PathVariable("comment_id") Long commentId){
          commentService.commentDelete(commentId);
--- src/main/java/com/project/instagramclone/web/comment/controller/NestedCommentController.java
+++ src/main/java/com/project/instagramclone/web/comment/controller/NestedCommentController.java
@@ -7,6 +7,8 @@ import com.project.instagramclone.security.UserDetailsImpl;
 import com.project.instagramclone.service.NestCommentService;
 import com.project.instagramclone.web.comment.dto.CommentSaveDto;
 import com.project.instagramclone.web.comment.dto.CommentUpdateDto;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -17,10 +19,12 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@Api(tags = {"대댓글"})
 public class NestedCommentController {
 
     private final NestCommentService nestCommentService;
 
+    @ApiOperation(value = "대댓글 작성", notes = "대댓글 작성입니다. {parent_id}에는 대댓글을 작성할 부모 댓글 pk값입니다.")
     @PostMapping("/api/{parent_id}/nestedcomment")
     public void nestedCommentSave(@AuthenticationPrincipal UserDetailsImpl userDetails ,@PathVariable("parent_id")Long parentId,
                                   @RequestBody CommentSaveDto commentSaveDto){
@@ -30,12 +34,14 @@ public class NestedCommentController {
         nestCommentService.nestedCommentSave(user, parentId, commentSaveDto);
     }
 
+    @ApiOperation(value = "대댓글 수정", notes = "대댓글 수정입니다. {comment_id}에는 수정할 대댓글 pk값 입니다.")
     @PutMapping("/api/{comment_id}/nestedcomment")
     public void nestedCommentUpdate(@PathVariable("comment_id") Long commentId,
                                     @RequestBody CommentUpdateDto commentUpdateDto){
         nestCommentService.nestedCommentUpdate(commentId,commentUpdateDto);
     }
 
+    @ApiOperation(value = "대댓글 삭제", notes = "대댓글 삭제입니다. {comment_id}에는 삭제할 대댓글 pk값 입니다.")
     @DeleteMapping("/api/{comment_id}/nestedcomment")
     public void nestedCommentDelete(@PathVariable("comment_id") Long commentId){
         nestCommentService.nestedCommetDelete(commentId);
