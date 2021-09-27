7f45427cb560bdd246e44c1b3014ae0bcb8eb077
parkjunsun <qkrwnstns52@naver.com>
2021-09-27 15:13:20 +0900

    09/27 게시글 전체 조회 구현

--- src/main/java/com/project/instagramclone/web/post/dto/PostShowDto.java
+++ src/main/java/com/project/instagramclone/web/post/dto/PostShowDto.java
@@ -4,12 +4,13 @@ import com.project.instagramclone.domain.post.entity.Photo;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter @Setter
 public class PostShowDto {
 
     private String content;
-    private List<Photo> photoList;
+    private List<String> photoList = new ArrayList<>();
 
 }
