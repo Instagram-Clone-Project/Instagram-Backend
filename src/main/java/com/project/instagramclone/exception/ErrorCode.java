f73f50887811e88f6d4694754990a6d54c143017
SeungCheol <ssc6839@gmail.com>
2021-10-04 00:01:04 +0900

    비밀번호 변경 에러 코드 추가

--- src/main/java/com/project/instagramclone/exception/ErrorCode.java
+++ src/main/java/com/project/instagramclone/exception/ErrorCode.java
@@ -10,6 +10,9 @@ public enum ErrorCode {
 
     //400 BAD_REQUEST : 잘못된 요청
     MISMATCH_AUTH_CODE(HttpStatus.BAD_REQUEST, "인증코드가 일치하지 않습니다."),
+    MISMATCH_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
+    MISMATCH_OLD_PASSWORD(HttpStatus.BAD_REQUEST, "이전 비밀번호가 잘못 입력되었습니다. 다시 입력해주세요."),
+    MISMATCH_CONFIRM_PASSWORD(HttpStatus.BAD_REQUEST, "두 비밀번호가 일치하는지 확인하세요."),
 
     //404 NOT_FOUND : Resource 를 찾을 수 없음
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 유저 정보를 찾을 수 없습니다."),
