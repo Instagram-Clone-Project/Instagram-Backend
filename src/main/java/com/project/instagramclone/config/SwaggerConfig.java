c4dc25955935a70f20c1863f909991ea9588f693
SeungCheol <ssc6839@gmail.com>
2021-10-11 22:51:50 +0900

    Swagger 헤더 추가

--- src/main/java/com/project/instagramclone/config/SwaggerConfig.java
+++ src/main/java/com/project/instagramclone/config/SwaggerConfig.java
@@ -3,12 +3,18 @@ package com.project.instagramclone.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @EnableSwagger2
 @Configuration
@@ -23,8 +29,21 @@ public class SwaggerConfig {
 
     @Bean
     public Docket restApi() {
+
+        //JWT header 처리 용도
+        List<Parameter> global = new ArrayList<>();
+
+        global.add(new ParameterBuilder()
+                .name("Authorization")
+                .description("\"Bearer \" + Access Token")
+                .parameterType("header")
+                .required(false)
+                .modelRef(new ModelRef("string"))
+                .build());
+
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
+                .globalOperationParameters(global)
                 .select()
                 //Swagger를 적용할 클래스의 package명
                 .apis(RequestHandlerSelectors.basePackage("com.project.instagramclone"))
