3aa7ad43b8bfb9ba20df2c992d86d318f0347c48
모수혁 <48712198+Mosuhyeok@users.noreply.github.com>
2021-10-21 12:48:58 +0900

    follow, nestedCommnet - > ResponseEntity 양식으로

--- src/main/java/com/project/instagramclone/web/follow/controller/FollowController.java
+++ src/main/java/com/project/instagramclone/web/follow/controller/FollowController.java
@@ -5,10 +5,13 @@ import com.project.instagramclone.service.FollowService;
 import com.project.instagramclone.web.follow.dto.FollowCntDto;
 import com.project.instagramclone.web.follow.dto.FollowerListDto;
 import com.project.instagramclone.web.follow.dto.FollowingListDto;
+import com.project.instagramclone.web.user.dto.SuccessResponseDto;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,22 +27,25 @@ public class FollowController {
 
     @ApiOperation(value = "팔로우", notes = "팔로우 기능입니다. {toUserId}에는 팔로우 할 대상의 userId 값 입니다.")
     @PostMapping("/api/follow/{toUserId}")
-    public void save(@AuthenticationPrincipal PrincipalDetails userDetails, @PathVariable("toUserId") Long toUserId){
+    public ResponseEntity<SuccessResponseDto> save(@AuthenticationPrincipal PrincipalDetails userDetails, @PathVariable("toUserId") Long toUserId){
 
         followService.save(userDetails.getUser(), toUserId);
+        return new ResponseEntity<>(SuccessResponseDto.builder().message("팔로우 완료").build(), HttpStatus.OK);
     }
 
 
     @ApiOperation(value = "팔로우 목록 조회", notes = "{user_id}에 해당하는 유저의 팔로우,팔로워 수를 보여줍니다.")
     @GetMapping("/api/follow/{user_id}")
-    public FollowCntDto followList(@PathVariable("user_id") Long userId){
-        return followService.getFollowInfo(userId);
+    public ResponseEntity<FollowCntDto> followList(@PathVariable("user_id") Long userId){
+        return new ResponseEntity<>(followService.getFollowInfo(userId),HttpStatus.OK);
     }
 
     @ApiOperation(value = "언팔로우", notes = "{toUserId}에 해당하는 유저를 언팔로우 합니다.")
     @DeleteMapping("/api/unfollow/{toUserId}")
-    public void delete(@AuthenticationPrincipal PrincipalDetails userDetails, @PathVariable("toUserId") Long toUserId){
+    public ResponseEntity<SuccessResponseDto> delete(@AuthenticationPrincipal PrincipalDetails userDetails, @PathVariable("toUserId") Long toUserId){
         followService.delete(userDetails.getUser(), toUserId);
+        return new ResponseEntity<>(SuccessResponseDto.builder().message("언팔로우 완료").build(), HttpStatus.OK);
+
     }
 
     @ApiOperation(value = "팔로워목록", notes = "{userId}에 해당하는 유저의 팔로워목록을 보여줍니다.")
